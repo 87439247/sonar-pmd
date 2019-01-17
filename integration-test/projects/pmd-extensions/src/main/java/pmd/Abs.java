e961379e133d54ac83b13da8d793fca3aef4f247
yuanyue <yuanyue@staff.hexun.com>
2019-01-17 23:30:31 +0800

    p3c 规则添加,居然成功了

---
+++ integration-test/projects/pmd-extensions/src/main/java/pmd/Abs.java
@@ -0,0 +1,11 @@
+package pmd;
+
+public abstract class Abs {
+
+    static Long warn = 1l;
+
+    public static void main(String args[]) {
+        if (warn == 1)
+            System.out.println(warn);
+    }
+}
---
+++ sonar-pmd-plugin/src/test/java/org/sonar/plugins/pmd/PmdP3cRulesDefinitionTest.java
@@ -0,0 +1,67 @@
+/*
+ * SonarQube PMD Plugin
+ * Copyright (C) 2012-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plugins.pmd;
+
+import org.junit.jupiter.api.Test;
+import org.sonar.api.server.rule.RulesDefinition;
+import org.sonar.api.server.rule.RulesDefinition.Param;
+import org.sonar.api.server.rule.RulesDefinition.Rule;
+import org.sonar.plugins.pmd.rule.PmdP3cRulesDefinition;
+import org.sonar.plugins.pmd.rule.PmdUnitTestsRulesDefinition;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PmdP3cRulesDefinitionTest {
+
+    @Test
+    void test() {
+
+        PmdP3cRulesDefinition definition = new PmdP3cRulesDefinition();
+        RulesDefinition.Context context = new RulesDefinition.Context();
+        definition.define(context);
+        RulesDefinition.Repository repository = context.repository(PmdConstants.P3C_REPOSITORY_KEY);
+
+        assertThat(repository)
+                .isNotNull()
+                .hasFieldOrPropertyWithValue("name", PmdConstants.P3C_REPOSITORY_NAME)
+                .hasFieldOrPropertyWithValue("language", PmdConstants.LANGUAGE_KEY);
+
+        List<Rule> rules = repository.rules();
+        assertThat(rules).hasSize(50);
+
+        for (Rule rule : rules) {
+            assertThat(rule.key()).isNotNull();
+
+            assertThat(rule.internalKey()).isNotNull();
+            assertThat(rule.name()).isNotNull();
+            assertThat(rule.htmlDescription()).isNotNull();
+            assertThat(rule.severity()).isNotNull();
+
+            for (Param param : rule.params()) {
+                assertThat(param.name()).isNotNull();
+                assertThat(param.description())
+                        .overridingErrorMessage("Description is not set for parameter '" + param.name() + "' of rule '" + rule.key())
+                        .isNotNull();
+            }
+        }
+    }
+}
